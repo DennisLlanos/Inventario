ca3a72033d7750908a1eba0bb41bcf71ef29d2d2
DennisLlanos <118204901+DennisLlanos@users.noreply.github.com>
2023-02-03 12:30:37 -0500

    Add files via upload

---
+++ INVENTARIO/vistas/home.php
@@ -0,0 +1,5 @@
+<div class="container is-fluid">
+	<h1 class="title"><font face="Comic Sans MS,Bodoni MT Black">JMobile Service </font></h1>
+	<h2 class="subtitle">La solucion cerca de ti...!</h2>
+	<center><img src="./img/logoINVENTARIO.JPG" width="550" height="475"></center>
+</div>
